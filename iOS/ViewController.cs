4f430e8d04fb46e5d92d05dddcae465303b6a39d
senator <senator@ms49.url.com.tw>
2017-07-27 12:03:32 +0800

    remove

---
+++ iOS/ViewController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using UIKit;
+
+namespace Liu.iOS
+{
+    public partial class ViewController : UIViewController
+    {
+        
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Perform any additional setup after loading the view, typically from a nib.
+            Button.AccessibilityIdentifier = "myButton";
+            Button.TouchUpInside += delegate
+            {
+                this.PerformSegue("moveToMasterViewSegue",this);
+            };
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.		
+        }
+    }
+}
